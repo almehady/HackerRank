9180d6ae8065106eaa0c46fd9b9fe1dd6f14ad62
almehady <almehady@gmail.com>
2020-02-28 20:49:11 +0300

    Feat: compare triplets solved in python

---
+++ Compare_Triplets.py
@@ -0,0 +1,44 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the compareTriplets function below.
+def compareTriplets(a, b):
+    a_sum = 0
+    b_sum = 0
+    result=[0,0]
+    if a[0] > b[0]:
+        a_sum += 1
+    elif a[0] < b[0]:
+        b_sum += 1
+    if a[1] > b[1]:
+        a_sum += 1
+    elif a[1] < b[1]:
+        b_sum += 1
+    if a[2] > b[2]:
+        a_sum += 1
+    elif a[2] < b[2]:
+        b_sum += 1
+    result[0] = a_sum
+    result[1] = b_sum
+    return result
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    a = list(map(int, input().rstrip().split()))
+
+    b = list(map(int, input().rstrip().split()))
+
+    result = compareTriplets(a, b)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
+
